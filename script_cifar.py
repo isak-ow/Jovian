9035c81d6ed9f73051b28bdd42deef1848f10ab5
IsakOrlando <isakow@stud.ntnu.no>
2023-09-08 13:18:12 +1000

    fixed optimizer

--- script_cifar.py
+++ script_cifar.py
@@ -57,8 +57,7 @@ epochs = 8
 model = f.to_device(f.ResNet9(color_channels, num_classes), device)
 print(model)
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.1,
-                      momentum=0.9, weight_decay=5e-4)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.1, weight_decay=5e-4)
 
 def train(epoch):
     model.train()
