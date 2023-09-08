369b2269c45c82a0ab7c041c516613efa11fa329
IsakOrlando <isakow@stud.ntnu.no>
2023-09-08 12:15:22 +1000

    added the download for the cluster

--- script_cifar.py
+++ script_cifar.py
@@ -19,6 +19,10 @@ wandb.init(
     }
 )
 
+from torchvision.datasets.utils import download_url
+dataset_url = "https://s3.amazonaws.com/fast-ai-imageclas/cifar10.tgz"
+download_url(dataset_url, '.')
+
 # Extract from archive
 with tarfile.open('./cifar10.tgz', 'r:gz') as tar:
     tar.extractall(path='./data')
