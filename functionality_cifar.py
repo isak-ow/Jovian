df447b4c04c350610d5543932737de08b733a952
IsakOrlando <isakow@stud.ntnu.no>
2023-09-08 12:12:51 +1000

    wrote script for the cluster, logging to wandb, cluster_run_1

---
+++ functionality_cifar.py
@@ -0,0 +1,90 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as tt
+
+def get_default_device():
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+    
+
+stats = ((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+train_tfms = tt.Compose([tt.RandomCrop(32, padding=4, padding_mode='reflect'), 
+                         tt.RandomHorizontalFlip(), 
+                         # tt.RandomRotate
+                         # tt.RandomResizedCrop(256, scale=(0.5,0.9), ratio=(1, 1)), 
+                         # tt.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
+                         tt.ToTensor(), 
+                         tt.Normalize(*stats,inplace=True)])
+valid_tfms = tt.Compose([tt.ToTensor(), tt.Normalize(*stats)])
+
+class DeviceDataLoader():
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+        
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl: 
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
+    
+
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+class SimpleResidualBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
+        
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.relu1(out)
+        out = self.conv2(out)
+        return self.relu2(out) + x # ReLU can be applied before or after adding the input
+    
+def conv_block(in_channels, out_channels, pool=False):
+    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1), 
+              nn.BatchNorm2d(out_channels), 
+              nn.ReLU(inplace=True)]
+    if pool: layers.append(nn.MaxPool2d(2))
+    return nn.Sequential(*layers)
+
+class ResNet9(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+        
+        self.conv1 = conv_block(in_channels, 64)
+        self.conv2 = conv_block(64, 128, pool=True)
+        self.res1 = nn.Sequential(conv_block(128, 128), conv_block(128, 128))
+        
+        self.conv3 = conv_block(128, 256, pool=True)
+        self.conv4 = conv_block(256, 512, pool=True)
+        self.res2 = nn.Sequential(conv_block(512, 512), conv_block(512, 512))
+        
+        self.classifier = nn.Sequential(nn.MaxPool2d(4), 
+                                        nn.Flatten(), 
+                                        nn.Dropout(0.2),
+                                        nn.Linear(512, num_classes))
+        
+    def forward(self, xb):
+        out = self.conv1(xb)
+        out = self.conv2(out)
+        out = self.res1(out) + out
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.res2(out) + out
+        out = self.classifier(out)
+        return out
---
+++ script_cifar.py
@@ -0,0 +1,140 @@
+import os
+import tarfile
+from torch.utils.data import DataLoader
+import functionality_cifar as f
+from torchvision.datasets import ImageFolder
+import wandb
+import torch
+
+wandb.init(
+    # set the wandb project where this run will be logged
+    project="cluster_CIFAR10_0809",
+    name = "cluster_run_1",
+    
+    # track hyperparameters and run metadata
+    config={
+    "architecture": "ResNet9",
+    "dataset": "CIFAR-10",
+    "epochs": 8,
+    }
+)
+
+# Extract from archive
+with tarfile.open('./cifar10.tgz', 'r:gz') as tar:
+    tar.extractall(path='./data')
+    
+# Look into the data directory
+data_dir = './data/cifar10'
+print(os.listdir(data_dir))
+classes = os.listdir(data_dir + "/train")
+num_classes = len(classes)
+color_channels = 3
+
+print(classes)
+
+train_data = ImageFolder(data_dir+'/train', f.train_tfms)
+valid_data = ImageFolder(data_dir+'/test', f.valid_tfms)
+
+batch_size = 128
+train_dl = DataLoader(train_data, batch_size, shuffle=True, num_workers=4, pin_memory=True)
+test_dl = DataLoader(valid_data, batch_size*2, num_workers=4, pin_memory=True)
+
+device = f.get_default_device()
+
+train_loader = f.DeviceDataLoader(train_dl,device)
+test_loader = f.DeviceDataLoader(test_dl,device)
+
+weight_decay = 1e-4
+max_lr = 0.01
+grad_clip = 0.1
+epochs = 8
+
+model = f.to_device(f.ResNet9(color_channels, num_classes))
+print(model)
+criterion = f.F.cross_entropy()
+optimizer = f.torch.optim.Adam(model.parameters(), max_lr, weight_decay=weight_decay)
+
+def train(epoch):
+    model.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    for i, batch in enumerate(train_loader):
+        images, labels = batch
+        images = images.to(device)
+        labels = labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs,labels)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += labels.size(0)
+        correct += predicted.eq(labels).sum().item()
+        if not abs(train_loss) < 0.02:
+            wandb.log({"train_loss": train_loss})
+
+def test(epoch):
+    global best_acc
+    model.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for i, batch in enumerate(test_loader):
+            images, labels = batch
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += labels.size(0)
+            correct += predicted.eq(labels).sum().item()
+            if not abs(test_loss) < 0.02:
+                wandb.log({"test_loss": test_loss})
+
+        acc = 100.*correct/total
+        wandb.log({"acc": acc})
+    if acc > best_acc:
+        print('Saving..')
+        state = {
+            'net': model.state_dict(),
+            'acc': acc,
+            'epoch': epoch,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, './checkpoint/ckpt.pth')
+        best_acc = acc
+        wandb.log({"best_acc": best_acc})
+
+#training loop
+scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr, epochs=epochs, 
+                                                steps_per_epoch=len(train_loader))
+
+for epoch in range(epochs):
+    wandb.log({"epoch": epoch})
+    train(epoch)
+    test(epoch)
+    scheduler.step()
+    wandb.log({"learning_rate": float(scheduler.get_lr)})
+
+wandb.finish()
+#history += ff.fit(epochs, learning_rate, model, train_loader, val_loader)
+
+# # image = train_data.data[np.random.randint(0,2000)]
+# # print(image.shape)
+# # print(train_data.classes)
+# # plt.imshow(image)
+# # plt.show()
+
+# batch_size = 128
+
+# train_dl = DataLoader(train_data, batch_size, shuffle=True, num_workers=4, pin_memory=True)
+# test_dl = DataLoader(test_data, batch_size*2, num_workers=4,pin_memory=True)
+
