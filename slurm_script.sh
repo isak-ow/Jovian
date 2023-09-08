b289f6e07f67180558b9c0231e573e6bd4414373
IsakOrlando <isakow@stud.ntnu.no>
2023-09-08 12:23:10 +1000

    added slurm file

---
+++ slurm_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --job-name=script
+#SBATCH --cpus-per-task 1
+#SBATCH --mail-type=All
+#SBATCH --mail-user=i.wangensteen@uq.net.au
+#SBATCH -o out.txt
+#SBATCH -e err.txt
+#SBATCH --partition=vgpu
+#SBATCH --gres=gpu:1
+
+conda activate cluster_env
+
+python3 ~/Jovian/script_cifar.py
