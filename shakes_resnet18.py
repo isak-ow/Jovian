9d157c13cb14307c97cdd4d613aaece9712c7681
IsakOrlando <isakow@stud.ntnu.no>
2023-09-10 16:15:44 +1000

    hopefully fixed the size issue

--- shakes_resnet18.py
+++ shakes_resnet18.py
@@ -9,7 +9,7 @@ class BasicBlock(nn.Module):
     self.conv1 = nn.Conv2d(
         in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
     self.bn1 = nn.BatchNorm2d(planes)
-    self.conv2 = nn.Conv2d(planes, planes, kernel_size=3
+    self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
                            stride =1, padding=1, bias=False)
     self.bn2 = nn.BatchNorm2d(planes)
 
