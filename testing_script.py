37bc7e8ce8f99b2afcd61a535326a93ed937a03c
IsakOrlando <isakow@stud.ntnu.no>
2023-09-10 17:59:13 +1000

    changed partition to test

--- testing_script.py
+++ testing_script.py
@@ -10,6 +10,7 @@ device = u.get_default_device()
 model = torch.load('model.pth',map_location=device)
 data_dir = './data/cifar10'
 test_data = ImageFolder(data_dir+'/test', transforms.ToTensor())
+print(test_data.shape)
 
 #loading test_set onto GPU
 test_dl = DataLoader(test_data, 1000, num_workers=1, pin_memory=True)
